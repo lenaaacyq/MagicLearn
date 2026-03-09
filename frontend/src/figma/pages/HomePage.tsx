4b651b202e1d180fd35c11b67db2bbc4e41622de
lenaaacyq <cyqanne1001@gmail.com>
2026-03-10 02:38:07 +0800

    Revert "Raise left panel bottom edge to align with right stack"
    This reverts commit 0854d8939a42cca38650152a995e777b4abcc35a.

--- frontend/src/figma/pages/HomePage.tsx
+++ frontend/src/figma/pages/HomePage.tsx
@@ -32,7 +32,7 @@ export default function HomePage() {
       {/* 主内容区 - Bento 布局 */}
       <div className="pt-28 pb-32 h-screen flex gap-6">
         {/* 左侧 - Agent 伴随舱 */}
-        <div className="w-[40%] min-h-0 h-[calc(100%-92px)] xl:h-[calc(100%-88px)] self-start">
+        <div className="w-[40%] min-h-0">
           <MerlinGenerator />
         </div>
 
